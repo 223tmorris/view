53e6afbc618dd87536f4947ff541c8444e42eced
223tmorris <223tmorris@frhsd.com>
2023-03-17 08:52:17 -0400

    finished tutorial 3

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const app = Vue.createApp({
     data() {
         return {
+            url: 'http://www.thenetninja.co.uk',
             showBooks: true,
-            title: 'The Final Empire',
-            author: 'Brandon Sanderson',
-            age: 45
+            // title: 'The Final Empire',
+            // author: 'Brandon Sanderson',
+            // age: 45,
+            // x: 0,
+            // y: 0
+            books: [
+                {title: 'name of the wind', author: 'patrick rothfuss', img: 'assets/1.jpg', isFav: true },
+                {title: 'the way of kings', author: 'brandon sanderson', img: 'assets/2.jpg', isFav: false },
+                {title: 'the final empire', author: 'brandon sanderson', img: 'assets/3.jpg', isFav: true }
+            ]
         }
     },
     methods: {
@@ -16,12 +24,26 @@ const app = Vue.createApp({
         toggleShowBooks(){
             this.showBooks = !this.showBooks
         },
-        handleEvent(e){
-            console.log(e, e.type)
+        toggleFav(book){
+            book.isFav = !book.isFav
+        }
+        // handleEvent(e, data){
+        //     console.log(e, e.type)
+        //     if(data){
+        //         console.log(data)
+        //     }
+        // },
+        // handleMousemove(e){
+        //     this.x = e.offsetX
+        //     this.y = e.offsetY
+        // }
+    },
+    computed: {
+        filteredBooks(){
+            return this.books.filter((book) => book.isFav)
         }
     }
 })
 
 app.mount('#app')
 
-// video 3 timestamp 5:13
